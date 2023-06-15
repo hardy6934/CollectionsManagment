37325cca72015fd57ba6ee9676b088f935676d2b
smamedov451@gmail.com <smamedov451@gmail.com>
2023-06-15 11:48:20 +0300

    Добавьте файлы проекта.

---
+++ CollectionsManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollectionsManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
